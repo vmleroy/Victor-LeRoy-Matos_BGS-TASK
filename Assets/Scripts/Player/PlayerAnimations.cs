f8fce2b31238a049374fae631cef068e0253f7fd
Victor Le Roy <victorlrmatos@gmail.com>
2024-07-12 18:42:44 -0300

    feat: add player animation to outfits

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
 
-    private Animator _animator;
-
+    private Animator[] _animators;
 
     // Start is called before the first frame update
     void Start()
     {
-        _animator = GetComponent<Animator>();
+        _animators = GetComponentsInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        WalkAnimation();
+    }
+
+    void WalkAnimation () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+        foreach (var animator in _animators)
+        {
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+        }
     }
 }
