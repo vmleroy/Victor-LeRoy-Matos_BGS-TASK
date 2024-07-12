4834bfc68f11c0afc4104596444778f795e64984
Victor Le Roy <victorlrmatos@gmail.com>
2024-07-12 19:04:36 -0300

    feat: add change outfit script

---
+++ Assets/Scripts/Player/PlayerSpriteControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class PlayerSpriteControl : MonoBehaviour
+{
+
+    private SpriteLibrary[] _spriteLibraries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteLibraries = GetComponentsInChildren<SpriteLibrary>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WIP: CODE SIMILAR TO THIS TO BE IMPLEMENTED AT STORE
+        // if (Input.GetKeyDown(KeyCode.LeftAlt))
+        // {
+        //     Debug.Log("Changing to PlayerAnimation");
+        //     ChangeOutfitLibrary(Resources.Load<SpriteLibraryAsset>("Player/Sprites/PlayerAnimation"));
+        // }
+    }
+
+    public void ChangeOutfitLibrary (SpriteLibraryAsset index) {
+        foreach (var library in _spriteLibraries)
+        {
+            if (library.gameObject.name == "Outfit")
+            library.spriteLibraryAsset = index;
+        }
+    }
+}
